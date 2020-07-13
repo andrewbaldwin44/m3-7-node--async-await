07890430f7c3d3c333a3ef82877c2540c0eb56be
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-12 23:20:31 -0400

    Solution to exercise 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,9 @@
 const request = require('request-promise');
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  const response = await request({uri: 'https://api.tronalddump.io/random/quote',
+                                  headers: {"Accept": "application/json"}, json: true});
+  return response.value;
 };
+
+getTronaldDumpQuote().then(response => console.log(response));
